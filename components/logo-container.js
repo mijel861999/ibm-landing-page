850e1aa39ada348e41ef76210cb76db7870bb051
mijel <mijel.dev@gmail.com>
2022-09-18 22:30:51 -0500

    Agregando configuración de prettier e imagenes

--- components/logo-container.js
+++ components/logo-container.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import IbmLogo from "../public/ibm-logo-18914.png";
 
 const LogoContainer = () => {
-	return (
-		<div>Logo container</div>
-	)
-}
+  return (
+    <div className="w-full flex justify-center mt-16">
+      <div className="w-1/4">
+        <Image src={IbmLogo} />
+      </div>
+    </div>
+  );
+};
 
-export default LogoContainer
-
+export default LogoContainer;
