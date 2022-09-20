0f2ff8bd3a717ffcb668e95bb5fd189d7eb2f45f
mijel <mijel.dev@gmail.com>
2022-09-19 22:00:25 -0500

    feat: haciendo el pononente del logo de ibm

--- components/logo-container.js
+++ components/logo-container.js
@@ -1,15 +1,22 @@
-import React from "react";
-import Image from "next/image";
-import IbmLogo from "../public/ibm-logo-18914.png";
+import React from 'react'
+import Image from 'next/image'
+import IbmLogo from '../public/ibm-logo-18914.png'
+
+/**
+ * Componente del logo
+ * @component
+ * @LogoContainer
+ *
+ */
 
 const LogoContainer = () => {
   return (
     <div className="w-full flex justify-center mt-16">
-      <div className="w-1/4">
-        <Image src={IbmLogo} />
+      <div className="w-200px">
+        <Image width={500} height={184} src={IbmLogo} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LogoContainer;
+export default LogoContainer
