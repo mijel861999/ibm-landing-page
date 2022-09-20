df084b48a003d4bf9b9e8646f696cbfaad31b60b
mijel <mijel.dev@gmail.com>
2022-09-19 22:17:13 -0500

    feat: agregando estilos a laptop-container

--- components/laptop-container.js
+++ components/laptop-container.js
@@ -1,17 +1,22 @@
-import React from "react";
-import Image from "next/image";
-import Thinkpad from "../public/laptop-ibm.png";
+import React from 'react'
+import Image from 'next/image'
+import Thinkpad from '../public/laptop-ibm.png'
 
 const LaptopContainer = () => {
   return (
-    <div className="w-full flex-col mt-16">
-			<h1>Reserva la nueva thinkpad</h1>
-			<p>Prueba la nueva tecnología con la que llevarás tus productos al siguiente nivel</p>
-      <div className="w-1/4">
-        <Image src={Thinkpad} />
+    <div className="w-full flex flex-col mt-16 justify-start items-center">
+      <h1 className="text-4xl font-bold mb-5 text-center">
+        Reserva la nueva thinkpad
+      </h1>
+      <p className="text-lg text-center">
+        Prueba la nueva tecnología con la que llevarás <br></br> tus productos
+        al siguiente nivel
+      </p>
+      <div className="mt-5">
+        <Image width={400} height={354} src={Thinkpad} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LaptopContainer;
+export default LaptopContainer
